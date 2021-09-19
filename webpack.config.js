05613f88f69e4fc4fc47f54b34176801d590d04f
Yoshiaki Togami <tuabobo123@gmail.com>
2021-09-20 00:31:18 +0900

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const ForkTsChecker = require("fork-ts-checker-webpack-plugin");
+
+module.exports = {
+  entry: {
+    bundle: [path.resolve(__dirname, "src/index.tsx")],
+  },
+  output: {
+    filename: "[name].js",
+    path: path.join(__dirname, "dist"),
+    chunkFilename: "[name].js",
+  },
+  module: {
+    rules: [
+      {
+        test: /\.ts(x?)$/,
+        use: [
+          {
+            loader: "ts-loader",
+            options: {
+              transpileOnly: true,
+            },
+          },
+        ],
+        exclude: /node_modules/,
+      },
+      {
+        test: /\.mjs$/,
+        include: /node_modules/,
+        type: "javascript/auto",
+      },
+    ],
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js", ".jsx"],
+  },
+  plugins: [
+    new ForkTsChecker(),
+    new HtmlWebpackPlugin({
+      template: `${__dirname}/src/template/index.html`,
+      filename: "index.html",
+      chunks: ["bundle"],
+      inject: "body",
+      scriptLoading: "defer",
+    }),
+  ],
+};
