4fe8fb93203ce70204512bb4906209864f95dcd7
Yoshiaki Togami <tuabobo123@gmail.com>
2021-09-20 01:07:22 +0900

    feat: impl basic logic

--- src/components/Playground.tsx
+++ src/components/Playground.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import AceEditor from 'react-ace';
 import styled from 'styled-components';
+import { parse } from '@astrojs/parser';
 
 export const Playground: React.FC = () => {
+  const [code, setCode] = useState<string>('');
+  const [parsedCode, setParsedCode] = useState<string>('');
+
+  const editCode = (value: string) => {
+    setCode(value);
+  };
+
+  useEffect(() => {
+    const parseCode = async () => {
+      try {
+        const newValue = parse(code);
+        setParsedCode(JSON.stringify(newValue));
+      } catch (error) {
+        setParsedCode(error as string);
+      }
+    };
+    parseCode();
+  }, [code]);
+
   return (
     <PlaygroundContainer>
-      <AceEditor editorProps={{ $blockScrolling: true }} />;
-      <textarea readOnly />
+      <AceEditor onChange={editCode} editorProps={{ $blockScrolling: true }} />
+      <textarea value={parsedCode} readOnly />
     </PlaygroundContainer>
   );
 };
